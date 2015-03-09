007c66f995050e50d30aa94397bc53b40ce52ae3
abaumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-03-09 12:48:46 -0700

    GameItem
    Changed GameItem to an interface

--- DungeonCrawler/src/game_Items/GameItem.java
+++ DungeonCrawler/src/game_Items/GameItem.java
@@ -1,20 +1,8 @@
 package game_Items;
 
-public abstract class GameItem {
-	private String itemName;
-	private int cost;
+public interface GameItem {
+	String getItemName();
 
-	protected void setItemName(String str, int amount) {
-		this.itemName = str;
-		this.cost = amount;
-	}
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public int getCost() {
-		return this.cost;
-	}
+	public int getCost();
 
 }
--- DungeonCrawler/src/game_Items/PotionOfHealth.java
+++ DungeonCrawler/src/game_Items/PotionOfHealth.java
@@ -1,8 +1,20 @@
 package game_Items;
 
 public class PotionOfHealth extends HealPotion {
+	private final String potionName = "Potion of Health";
+	private final int potionCost = 2;
+
 	public PotionOfHealth() {
-		super.setItemName("Potion of Health", 2);
-		super.setHealAmount(2);
+		this.setHealAmount(2);
+	}
+
+	@Override
+	public int getCost() {
+		return this.potionCost;
+	}
+
+	@Override
+	public String getItemName() {
+		return this.potionName;
 	}
 }
